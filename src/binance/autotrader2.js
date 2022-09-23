3afdbb0fe752116006ff52ac92f713548330e6a7
kim <kimh0413@naver.com>
2022-09-23 17:33:32 +0900

    0923

--- src/binance/autotrader2.js
+++ src/binance/autotrader2.js
@@ -16,15 +16,22 @@ class AutoTrader2 {
         this.autoBuy();
     }
 
+    stop(){
+        
+    }
+
     watch(){
         for (symbol in crossFlag){
-            
+            //poll the server to check if obv240 is crossed.
+            //if crossed, set the cross flag and fire an crossed event.   
         }
     }
     autoBuy(){
-
+        //if the cross event occured and the prediction condition is met,
+        //open the websocket stream to monitor obv15 and obv5 
+        //if all the conditions are met place an buy order with the 2% stop.
     }
     sell(){
-
+        //monitor the buy history list and crossFlag list to check if the sell condition is met.
     }
 }
