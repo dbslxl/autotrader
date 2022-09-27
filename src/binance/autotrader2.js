14dfc5e10932cff26125e12b3c6ec20d3b90c08e
kim <kimh0413@naver.com>
2022-09-27 17:38:09 +0900

    0927

--- src/binance/autotrader2.js
+++ src/binance/autotrader2.js
@@ -1,64 +1,68 @@
 const WebSocket = require('ws')
 const axios = require('axios')
 
-const binanceURL = 'wss://dstream.binance.com/ws/btcusd_perp@kline_4h'
+//const binanceURL = 'wss://dstream.binance.com/ws/btcusd_perp@kline_4h'
+const binaceAPIURL = 'https://fapi.binance.com'
 
-class AutoTrader2 {
-    constructor(symbols) {
-        this.isWatching = false
-        this.crossFlagList=[]
-
-        for (symbol of symbols){
-            //fetch obv240 data of the symbols from the api
-            //setup the crossFlag list
-            //this.crossFlag=[{symbol:'ethusd_perp',prev_cross:'up',cross:''},{symbol:'btcusd_perp',prev_cross:'up',cross:'up'}]            
-        }
-    }
-
-    test(){
-        for (crossFlag of crossFlagList){
-
-        }
-
-        this.crossFlagList.filter((data)=>{
-            data.cross
+class AutoTrader {
+    constructor(symbol) {
+        this.symbol=symbol
+        this.prevCross=''
+        
+        this.on('upcross',()=>{
+            this.Obv15mTimer&&clearInterval(this.Obv15mTimer);
+            this.Obv5mTimer=setInterval(this.checkObv5m,1000)
         })
-    }
+        this.on('donwcross',()=>{
+            this.Obv5mTimer&&clearInterval(this.Obv5mTimer);    
+            this.Obv15mTimer=setInterval(this.checkObv15m,1000)
+        })
+       
+        
+        // this.isWatching = false
+        // this.crossFlagList=[]
 
+    }
+  
     run(){
-        this.watch();
-        this.autoBuy();
+        this.OBV1dTimer=setInterval(checkOBV1d,1000)
     }
 
     stop(){
-        
+        clearInterval(this.OBV1dTimer&&this.OBV1dTimer)
     }
-    watch(){
-        for (symbol in crossFlag){
-            //poll the api server to check if obv240 is crossed.
-            //if crossed, set the cross flag and fire an crossed event.   
+   
+    calculateObv(){
+
+    }
+    async checkObv1d(){
+        //check 1d obv by polling the api
+        response = await axios.get(`https://fapi.binance.com/fapi/v1/klines?symbol=${this.symbol}&interval=1d&limit=10`)
+        if(calculateObv(response.data)>0){
+            //upcross 시 업크로스 이벤트 emit
+            if(this.prevCross!=='up'){
+                this.prevCross='up'
+                this.emit('upcross')
+            }
+        }else if(calculateObv(response.data)<0){
+            //downcross 시 다운크로스 이벤트 emit
+            if(this.prevCross!=='down'){
+                this.prevCross='down'
+                this.emit('downcross')
+            }
         }
     }
-    autoBuy(){
-        //if the cross event occured and the prediction condition is met,
-        //open the websocket stream to monitor obv15 and obv5 
-        //if all the conditions are met place an buy order with the 2% stop.
+    checkObv5m(){
+        //5분 obv upcross 체크
+        //현재 보유중인 포지션이 없으면 long 매수 
+        //foot 매도중이면 foot매수후 long 매수
+        clearInterval(this.Obv5mTimer)        
     }
-    sell(){
-        //monitor the buy history list and crossFlag list to check if the sell condition is met.
+    checkObv15m(){
+        //15분 obv downcross 체크
+        //현재 보유중인 포지션이 없으면 foot 매도
+        //long 매수중이면 long매도후 foot 매도
+        clearInterval(this.Obv15mTimer)
     }
+    
 }
-
-class AutoBuyer {
-    constructor(){
-
-    }
-}
-
-class AutoSeller {
-    constructor(){
-
-    }
-}
-
-//evet type : upcross, downcross, buy, sell 
--- src/binance/test.js
+++ src/binance/test.js
@@ -13,6 +13,6 @@ const axios = require('axios')
 // axios.get('https://dapi.binance.com/dapi/v1/klines?symbol=btcusd_perp&interval=5m&limit=1500')
 //     .then((response)=>console.log(response.data,response.data.length))
 
-let crossFlagList=[{symbol:'ethusd_perp',prev_cross:'up',cross:''},{symbol:'btcusd_perp',prev_cross:'up',cross:'up'},{symbol:'btcusd_perp',prev_cross:'up',cross:'up'}]
+let crossFlagList=[{symbol:'ethusd_perp',prev_cross:'up',cross:''},{symbol:'btcusd_perp',prev_cross:'up',cross:'up'},{symbol:'btcusd_perp',prev_cross:'up',cross:'up'},{symbol:'btcusd_perp',prev_cross:'up'}]
 
-console.log(crossFlagList.filter(crossFlag.cross))
+console.log(crossFlagList.filter(crossFlag=>!crossFlag.cross))
