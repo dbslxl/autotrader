14bcee08d79465783ed7260b4a12ba216cb00950
kim <kimh0413@naver.com>
2022-10-25 16:26:15 +0900

    remove unnecessary files

--- src/binance/test2.js
+++ src/binance/test2.js
@@ -2,16 +2,16 @@ const axios = require('axios')
 const EventEmitter = require('events')
 const Binance = require('node-binance-api')
 //사장님
-const binance=new Binance().options({
-    APIKEY:'5eD1LQCYKEdOyYuOeZWavfFWzhecoDMFBxBoUniBCXHRqBPECawXEU1JTeAVZllB',
-    APISECRET:'DdRzV1XKoZAPlzpjALQzjNlh8uuYuoFqr6OAsK8jIqF9NmCAk7iEIJvRItOHx3E7'
-})
+// const binance=new Binance().options({
+//     APIKEY:'5eD1LQCYKEdOyYuOeZWavfFWzhecoDMFBxBoUniBCXHRqBPECawXEU1JTeAVZllB',
+//     APISECRET:'DdRzV1XKoZAPlzpjALQzjNlh8uuYuoFqr6OAsK8jIqF9NmCAk7iEIJvRItOHx3E7'
+// })
 
 // 실장님
-// const binance=new Binance().options({
-//     APIKEY:'A4nOHmYpEL9T73QKVGcr5ZKE2WiOtzdHZ9G2iCWaDGVMvXTbfeyrYIJeyltn8SSc',
-//     APISECRET:'SuT2cu12uIhjB9mCeGj3D4TGM2EeiN8tNAKDxCXWswHz3T2SFZimK5bgUGLFyVmS'
-// })
+const binance=new Binance().options({
+    APIKEY:'A4nOHmYpEL9T73QKVGcr5ZKE2WiOtzdHZ9G2iCWaDGVMvXTbfeyrYIJeyltn8SSc',
+    APISECRET:'SuT2cu12uIhjB9mCeGj3D4TGM2EeiN8tNAKDxCXWswHz3T2SFZimK5bgUGLFyVmS'
+})
 
 // const binance=new Binance().options({
 //     APIKEY:'rQkKYkK7sa286zYyjqvygn8J3O6UXGydLDeRvhOdUUx8G1MMh0TPp5RiRJ9QG7xL',
